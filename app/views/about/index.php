5f92999a8eb42485bd629d001afe116e14c1999c
WahyuAprian <193040141.wahyu@mail.unpas.ac.id>
2021-10-01 15:11:36 +0700

    Membuat dan menambahkan file sesuai dengan video ke-6 PHP MVC pertemuan ke-4

--- app/views/about/index.php
+++ app/views/about/index.php
@@ -1,2 +1,5 @@
-<h1>About Me</h1>
-<p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL; ?>/img/193040141.jpg" alt="Wahyu Aprian" width="150" class="shadow">
+  <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?> tahun, saya adalah seorang <?= $data['pekerjaan']; ?>.</p>
+</div>
